845c584e9deefbe5857b26c2f35120baa9332ea6
vmnghia <nghiavm187@gmail.com>
2024-11-11 14:46:41 +0700

    build: update config
    Signed-off-by: vmnghia <nghiavm187@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,22 @@
 import typescript from 'rollup-plugin-typescript2';
 
 /** @type {import('rollup').RollupOptions} */
-export default {
-  input: './src/index.ts',
-  output: {
-    dir: 'dist',
-    format: 'es',
+export default [
+  {
+    input: './src/index.ts',
+    output: {
+      dir: 'dist',
+      format: 'umd',
+      name: 'FragilePromise'
+    },
+    plugins: [typescript()],
   },
-  plugins: [typescript()],
-};
+  {
+    input: './src/index.ts',
+    output: {
+      file: 'dist/index.mjs',
+      format: 'es',
+    },
+    plugins: [typescript()],
+  },
+];
